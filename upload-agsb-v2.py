eb91c7e635d2ad1122ff72c4544d10e189610415
mytea-git <2943969603@qq.com>
2025-06-06 18:36:18 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "mainking"         # 用户名
+UUID = "b2fed810-80a4-4e7a-9f1e-0fb19459d8db"                     # UUID，留空则自动生成
+PORT = 10699                   # Vmess端口，留空或0则自动生成
+DOMAIN = "https://voo.birthdaycc.dpdns.org/"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiOWE2ZWJiNTQzODFkMWMyMDIzM2NkNWM0OGE4NjMyOWUiLCJ0IjoiY2Y4NGU1MTItYmI2Yi00NmU4LWJiMmYtODdlMjYxMmJjNTk1IiwicyI6Ik9EWmtZek0xTldJdE5URTNPUzAwTlRZeUxUbGpOekF0WXpFNU9XTmlZamhqT1RjeCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
